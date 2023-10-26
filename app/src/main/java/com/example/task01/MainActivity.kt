52e648dbd11a4c5da5e4ca8c99c0f3a3daaec1bf
dev-6 <tryus.dev6@gmail.com>
2023-10-26 09:20:34 +0600

    large file download app with notification progress done

--- app/src/main/java/com/example/task01/MainActivity.kt
+++ app/src/main/java/com/example/task01/MainActivity.kt
@@ -2,10 +2,8 @@ package com.example.task01
 
 import DownloadWorker
 import android.Manifest
-import android.app.DownloadManager
-import android.app.NotificationChannel
+import android.app.Activity
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -13,23 +11,20 @@ import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
-import android.widget.Button
 import android.widget.Toast
-import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.work.Data
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import com.example.task01.databinding.ActivityMainBinding
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,36 +33,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var notificationBuilder : NotificationCompat.Builder
     private lateinit var notificationManager : NotificationManagerCompat
+    private var currentProgress = 0
+    val REQUEST_CODE_PERMISSIONS = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        notificationBuilder = NotificationCompat.Builder(this, "download_channel")
-        notificationManager = NotificationManagerCompat.from(this)
+        requestPermissions()
 
-        //val downloadButton = findViewById<Button>(R.id.downloadBtn)
-
-//        viewModel = ViewModelProvider(this)[FileDownloadViewModel::class.java]
-
-//        viewModel.downloadProgress.observe(this) { progress ->
-//            //updateNotificationProgress(progress)
-//            Log.d("NOTIFICATION", " $progress")
-//        }
-
-
-//        viewModel.downloadProgress.observe(
-//            this
-//        ) {
-//            Log.d("NOTIFICATION", it.toString())
-//        }
-//
-//        viewModel.updateProgress(0)
 
 
         binding.downloadBtn.setOnClickListener {
-            val url = "https://file-examples.com/storage/fee82f638f6538df295a1ac/2017/04/file_example_MP4_1920_18MG.mp4"
+            val url = "https://file-examples.com/storage/fe1134defc6538ed39b8efa/2017/04/file_example_MP4_1920_18MG.mp4"
             val workData = Data.Builder().putString("url", url).build()
 
             val downloadRequest = OneTimeWorkRequest.Builder(DownloadWorker::class.java)
@@ -82,7 +61,7 @@ class MainActivity : AppCompatActivity() {
                             val outputData = workInfo.outputData
                             val exceptionMessage = outputData.getString("exceptionMessage")
                             if (exceptionMessage != null) {
-                                // Handle the exception message, e.g., show a toast with the error
+
                                 Toast.makeText(applicationContext, exceptionMessage, Toast.LENGTH_SHORT).show()
                             }
                         }
@@ -99,48 +78,51 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
+    private fun checkAndRequestPermissions(activity: Activity, vararg permissions: String): Boolean {
+        val permissionsList: MutableList<String> = ArrayList()
+        for (permission in permissions) {
+            val permissionState = activity.checkSelfPermission(permission)
+            if (permissionState == PackageManager.PERMISSION_DENIED) {
+                permissionsList.add(permission)
+            }
+        }
+        if (!permissionsList.isEmpty()) {
+            ActivityCompat.requestPermissions(
+                activity,
+                permissionsList.toTypedArray<String>(),
+                REQUEST_CODE_PERMISSIONS
+            )
+            return false
+        }
+        return true
+    }
+    private fun requestPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            checkAndRequestPermissions(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.POST_NOTIFICATIONS,
+                Manifest.permission.READ_MEDIA_IMAGES,
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_CALENDAR,
+                Manifest.permission.RECORD_AUDIO
+            )
+        }
+    }
     override fun onResume() {
         super.onResume()
         val filter = IntentFilter("progress_update")
         registerReceiver(progressReceiver, filter)
     }
-
-
-
     override fun onPause() {
         super.onPause()
-//        notificationBuilder.setContentTitle("Download in Progress")
-//            .setSmallIcon(android.R.drawable.ic_dialog_info)
-//            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-        //showNotification(progressReceiver.resultData.length)
-        //unregisterReceiver(progressReceiver)
+
         notificationBuilder = NotificationCompat.Builder(this, "download_channel")
         notificationManager = NotificationManagerCompat.from(this)
 
-        notificationBuilder.setContentTitle("Download in Progress")
-            .setSmallIcon(android.R.drawable.ic_dialog_info)
-        val notificationId = 1
-
-
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.POST_NOTIFICATIONS
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
-        }
-        notificationManager.notify(notificationId, notificationBuilder.build())
+        showNotification(currentProgress)
 
     }
-
     private fun updateDownloadProgress(progress: Int) {
         Log.d("Progress", "$progress%")
         binding.progressBar.progress = progress
@@ -150,9 +132,6 @@ class MainActivity : AppCompatActivity() {
             binding.progress.text = "Download Complete"
         }
 
-        notificationBuilder
-            .setProgress(100, progress, false)
-            .setContentText("Downloading... $progress%")
 
     }
 
@@ -162,20 +141,32 @@ class MainActivity : AppCompatActivity() {
             if (progress != null) {
                 updateDownloadProgress(progress)
 
-                showNotification(progress)
+
+                currentProgress = progress
             }
         }
     }
 
     private fun showNotification(progress: Int) {
-        notificationBuilder
-            .setProgress(100, progress, false)
-            .setContentText("Downloading... $progress%")
+
+        notificationBuilder.setContentTitle("Download in Progress")
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setContentText("Downloading... $currentProgress%")
+            .setProgress(100, currentProgress, false)
             .setPriority(NotificationCompat.PRIORITY_LOW)
             .setOngoing(true)
+        val notificationId = 1
 
 
-        // Use a unique notification ID to ensure each notification is displayed
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        notificationManager.notify(notificationId, notificationBuilder.build())
+
     }
 
 }
